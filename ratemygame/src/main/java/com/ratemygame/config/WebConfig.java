2ac04f53135e124f3021968c4f4ad1bbad25f294
Cédric <cedric.andre.martins@everis.com>
2018-09-13 22:35:59 +0100

    add basic security

--- ratemygame/src/main/java/com/ratemygame/config/WebConfig.java
+++ ratemygame/src/main/java/com/ratemygame/config/WebConfig.java
@@ -2,14 +2,28 @@ package com.ratemygame.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration 
 public class WebConfig implements WebMvcConfigurer  {  
 
 	    @Override
 	    public void addViewControllers(ViewControllerRegistry registry) {
-	        registry.addRedirectViewController("/", "/mainpage");
+	    	
+	        registry.addRedirectViewController("/", "/homepage");
+	        registry.addViewController("/login.html");
+	    }
+	    
+	    @Override
+	    public void configureViewResolvers(ViewResolverRegistry registry) {
+	        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	        resolver.setPrefix("/WEB-INF/view/");
+	        resolver.setSuffix(".jsp");
+	        resolver.setViewClass(JstlView.class);
+	        registry.viewResolver(resolver);
 	    }
 
 
--- ratemygame/src/main/java/com/ratemygame/main/controllers/MainController.java
+++ ratemygame/src/main/java/com/ratemygame/main/controllers/MainController.java
@@ -1,18 +1,19 @@
 package com.ratemygame.main.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class MainController {
 	
-	@GetMapping("/mainpage")
+	@GetMapping("/homepage")
 	public String getMainPage(Model model) {
-		return "mainpage";
+		return "homepage";
 	}
 
 }
